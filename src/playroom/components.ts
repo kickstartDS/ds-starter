9db7405fe2fcfac8b1574aea77495d9564d28f15
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2022-11-14 20:12:15 +0100

    fix: fix playroom component imports

--- src/playroom/components.ts
+++ src/playroom/components.ts
@@ -4,5 +4,29 @@ export { ContentBox } from "../components/content-box/ContentBoxComponent";
 
 export * from "@bedrock-layout/primitives";
 
-export * from "@kickstartds/base";
-export * from "@kickstartds/content";
+export { CollapsibleBox } from "@kickstartds/base/lib/collapsible-box";
+export { Contact } from "@kickstartds/base/lib/contact";
+export { Divider } from "@kickstartds/base/lib/divider";
+export { Headline } from "@kickstartds/base/lib/headline";
+export { Html } from "@kickstartds/base/lib/html";
+export { Icon } from "@kickstartds/base/lib/icon";
+export { IframeRatio } from "@kickstartds/base/lib/iframe";
+export { LightboxImage } from "@kickstartds/base/lib/lightbox-image";
+export { Link } from "@kickstartds/base/lib/link";
+export { Picture } from "@kickstartds/base/lib/picture";
+export { RichText } from "@kickstartds/base/lib/rich-text";
+export { Table } from "@kickstartds/base/lib/table";
+export { TagLabel } from "@kickstartds/base/lib/tag-label";
+export { TagLabelContainer } from "@kickstartds/base/lib/tag-label-container";
+export { Teaser } from "@kickstartds/base/lib/teaser";
+export { TeaserBox } from "@kickstartds/base/lib/teaser-box";
+export { TeaserRow } from "@kickstartds/base/lib/teaser-row";
+export { TextMedia } from "@kickstartds/base/lib/text-media";
+
+export { CountUp } from "@kickstartds/content/lib/count-up";
+export { LogoTiles } from "@kickstartds/content/lib/logo-tiles";
+export { Quote } from "@kickstartds/content/lib/quote";
+export { QuotesSlider } from "@kickstartds/content/lib/quotes-slider";
+export { Storytelling } from "@kickstartds/content/lib/storytelling";
+export { Visual } from "@kickstartds/content/lib/visual";
+export { VisualSlider } from "@kickstartds/content/lib/visual-slider";
